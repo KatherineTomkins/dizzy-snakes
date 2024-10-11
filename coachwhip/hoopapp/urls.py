ad8ff126ae8e4ab8478d04346e9041f6a78d66c9
KatherineTomkins <ktomkins@DESKTOP-5U0FN7J>
2024-10-11 15:27:55 +0100

    added the halloumi section to django project

--- coachwhip/hoopapp/urls.py
+++ coachwhip/hoopapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hoopview/", views.hoopview)
+    path("hoopview/", views.hoopview),
+    path("halloumi", views.halloumi)
 ]
--- coachwhip/hoopapp/views.py
+++ coachwhip/hoopapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def hoopview(request):
     return render(request, 'index.html')
+
+def halloumi(request):
+    return render(request, 'halloumi.html')
